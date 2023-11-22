37a7b055eec024559712ffb3266b03d547876210
Taylan Özdoğan <ozdogantaylan@gmail.com>
2023-11-23 02:51:15 +0300

    Initial comit.

---
+++ cmake-build-debug/CMakeFiles/blockfall.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/taylan/CLionProjects/pa2/blockfall/BlockFall.cpp" "CMakeFiles/blockfall.dir/BlockFall.cpp.o" "gcc" "CMakeFiles/blockfall.dir/BlockFall.cpp.o.d"
+  "/Users/taylan/CLionProjects/pa2/blockfall/GameController.cpp" "CMakeFiles/blockfall.dir/GameController.cpp.o" "gcc" "CMakeFiles/blockfall.dir/GameController.cpp.o.d"
+  "/Users/taylan/CLionProjects/pa2/blockfall/Leaderboard.cpp" "CMakeFiles/blockfall.dir/Leaderboard.cpp.o" "gcc" "CMakeFiles/blockfall.dir/Leaderboard.cpp.o.d"
+  "/Users/taylan/CLionProjects/pa2/blockfall/LeaderboardEntry.cpp" "CMakeFiles/blockfall.dir/LeaderboardEntry.cpp.o" "gcc" "CMakeFiles/blockfall.dir/LeaderboardEntry.cpp.o.d"
+  "/Users/taylan/CLionProjects/pa2/blockfall/main.cpp" "CMakeFiles/blockfall.dir/main.cpp.o" "gcc" "CMakeFiles/blockfall.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
