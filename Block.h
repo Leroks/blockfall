73a1015663f265018f430ea3c5d0277b9cfff84e
Taylan Özdoğan <ozdogantaylan@gmail.com>
2023-11-26 13:25:22 +0300

    35.68

--- Block.h
+++ Block.h
@@ -9,18 +9,18 @@ class Block {
 public:
 
     vector<vector<bool>> shape; // Two-dimensional vector corresponding to the block's shape
-    Block * right_rotation = nullptr; // Pointer to the block's clockwise neighbor block (its right rotation)
-    Block * left_rotation = nullptr; // Pointer to the block's counter-clockwise neighbor block (its left rotation)
-    Block * next_block = nullptr; // Pointer to the next block to appear in the game
+    Block *right_rotation = nullptr; // Pointer to the block's clockwise neighbor block (its right rotation)
+    Block *left_rotation = nullptr; // Pointer to the block's counter-clockwise neighbor block (its left rotation)
+    Block *next_block = nullptr; // Pointer to the next block to appear in the game
 
-    bool operator==(const Block& other) const {
+    bool operator==(const Block &other) const {
         // TODO: Overload the == operator to compare two blocks based on their shapes
-        if(shape[0].size() != other.shape[0].size() || shape.size() != other.shape.size()){
+        if (shape[0].size() != other.shape[0].size() || shape.size() != other.shape.size()) {
             return false;
         }
-        for(int i = 0; i < shape.size(); i++){
-            for(int j = 0; j < shape[0].size(); j++){
-                if(shape[i][j] != other.shape[i][j]){
+        for (int i = 0; i < shape.size(); i++) {
+            for (int j = 0; j < shape[0].size(); j++) {
+                if (shape[i][j] != other.shape[i][j]) {
                     return false;
                 }
             }
@@ -29,20 +29,66 @@ public:
         return true;
     }
 
-    bool operator!=(const Block& other) const {
+    bool operator!=(const Block &other) const {
         // TODO: Overload the != operator to compare two blocks based on their shapes
-        if(shape[0].size() != other.shape[0].size() || shape.size() != other.shape.size()){
+        if (shape[0].size() != other.shape[0].size() || shape.size() != other.shape.size()) {
             return true;
         }
-        for(int i = 0; i < shape.size(); i++){
-            for(int j = 0; j < shape[0].size(); j++){
-                if(shape[i][j] != other.shape[i][j]){
+        for (int i = 0; i < shape.size(); i++) {
+            for (int j = 0; j < shape[0].size(); j++) {
+                if (shape[i][j] != other.shape[i][j]) {
                     return true;
                 }
             }
         }
         return false;
     }
+
+    static vector<vector<bool>> rotate90degrees(const vector<vector<bool>> &inputShapeGiven) {
+        vector<vector<bool>> result;
+        for (int i = inputShapeGiven[0].size() - 1; i >= 0; --i) {
+            vector<bool> newRow;
+            newRow.reserve(inputShapeGiven.size());
+            for (int j = 0; j < inputShapeGiven.size(); ++j) {
+                newRow.push_back(inputShapeGiven[inputShapeGiven.size() - 1 - j][i]);
+            }
+            result.push_back(newRow);
+        }
+        return result;
+    }
+
+    bool isShapeInList(const vector<vector<bool>> &shapeGiven) {
+        Block *tempBlock = this;
+        while (tempBlock != nullptr) {
+            if (tempBlock->shape == shapeGiven) {
+                return true;
+            }
+            tempBlock = tempBlock->right_rotation;
+        }
+        return false;
+    }
+
+
+    void createCircularLinkedList() {
+        Block *start = this;
+        Block *current = start;
+        vector<vector<bool>> tempShape = rotate90degrees(start->shape);
+
+        for (int i = 0; i < 3; ++i) {
+            if (!isShapeInList(tempShape)) {
+                current->right_rotation = new Block;
+                current->right_rotation->shape = tempShape;
+                current->right_rotation->next_block = start->next_block;
+                current->right_rotation->left_rotation = current;
+                current = current->right_rotation;
+            }
+            tempShape = rotate90degrees(tempShape);
+        }
+
+        current->right_rotation = start;
+        start->left_rotation = current;
+    }
+
 };
 
 
--- BlockFall.cpp
+++ BlockFall.cpp
@@ -1,4 +1,10 @@
 #include "BlockFall.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
 
 BlockFall::BlockFall(string grid_file_name, string blocks_file_name, bool gravity_mode_on, const string &leaderboard_file_name, const string &player_name) : gravity_mode_on(
         gravity_mode_on), leaderboard_file_name(leaderboard_file_name), player_name(player_name) {
@@ -12,14 +18,148 @@ void BlockFall::read_blocks(const string &input_file) {
     // TODO: For every block, generate its rotations and properly implement the multilevel linked list structure
     //       that represents the game blocks, as explained in the PA instructions.
     // TODO: Initialize the "power_up" member variable as the last block from the input file (do not add it to the linked list!)
+    fstream inputFile(input_file);
+    Block* curr = new Block;
+    Block * before ;
+    std::vector<bool> lineData;
+    bool isFile = false;
+
+    if (inputFile.is_open()) {
+
+        char tmp;
+        while (inputFile >> tmp){
+            if (tmp == '['){
+                isFile = true;
+                if (initial_block == nullptr){
+                    initial_block = curr;
+                }else{
+                    before = curr;
+                    curr = curr->next_block;
+
+                }
+                curr->shape.clear();
+                std::string line;
+                bool continueReading = true;
+                int kl = 0;
+                while (continueReading) {
+                    kl++;
+                    std::getline(inputFile, line);
+                    std::istringstream iss(line);
+                    std::string search = "]";
+                    size_t found = line.find(search);
+                    if (found != std::string::npos) {
+                        continueReading = false;
+
+                    }
+                    char num;
+                    while (iss >> num) {
+                        if (num == '1'){
+                            lineData.push_back(true);
+                        }else if (num == '0'){
+                            lineData.push_back(false);
+                        }else if (num == ']'){
+                            break;
+                        }
+                    }
+                    curr->shape.push_back(lineData);
+
+                    lineData.clear();
+
+                }
+                curr->next_block = new Block;
+                curr->createCircularLinkedList();
+            }
+
+
+        }
+    }else{
+        std::cout << "Failed to open the file." << std::endl;
+
+    }
+    if (!isFile){
+        delete curr;
+        return;
+    }
+    delete curr->next_block;
+    if (initial_block==curr){
+        initial_block = nullptr;
+    }
+    for (int i = 0 ; i < 4 ; i++){
+        curr->next_block = nullptr;
+        curr = curr->left_rotation;
+        if (before != nullptr){
+            before->next_block = nullptr;
+            before = before->left_rotation;
+        }
+
+    }
+    power_up = curr->shape;
+    //delete all rotations of the last block
+    if (initial_block!= nullptr){
+        active_rotation = initial_block;
+
+    }
+    curr->left_rotation->right_rotation = nullptr;
+    curr->left_rotation = nullptr;
+    while (curr != nullptr){
+        Block *tmp = curr;
+        curr = curr->right_rotation;
+        delete tmp;
+    }
+
 }
 
 void BlockFall::initialize_grid(const string &input_file) {
     // TODO: Initialize "rows" and "cols" member variables
     // TODO: Initialize "grid" member variable using the command-line argument 1 in main
+
+    fstream inputFile(input_file);
+    if(inputFile.is_open()){
+        string line;
+        getline(inputFile, line);
+        istringstream iss(line);
+        string num;
+        int count = 0;
+        while(iss >> num){
+            count++;
+        }
+        cols = count;
+        while(getline(inputFile, line)){
+            rows++;
+        }
+        inputFile.close();
+    }
+    fstream inputFile2(input_file);
+    if(inputFile2.is_open()){
+        string line;
+        while(getline(inputFile2, line)){
+            istringstream iss(line);
+            vector<int> lineData;
+            int num;
+            while(iss >> num){
+                lineData.push_back(num);
+            }
+            grid.push_back(lineData);
+        }
+        inputFile2.close();
+    }
 }
 
 
 BlockFall::~BlockFall() {
     // TODO: Free dynamically allocated memory used for storing game blocks
+    Block *tmp = initial_block;
+    while (tmp != nullptr){
+        Block *tmp2 = tmp;
+        tmp2->left_rotation->right_rotation = nullptr;
+        tmp2->left_rotation = nullptr;
+        tmp = tmp->next_block;
+        while (tmp2 != nullptr){
+            Block *tmp3 = tmp2;
+            tmp2 = tmp2->right_rotation;
+            //delete shape of tmp3 here
+
+            delete tmp3;
+        }
+    }
 }
