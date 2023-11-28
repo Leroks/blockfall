99562aa505886121179dff54264b4513f94ebeab
Taylan Özdoğan <ozdogantaylan@gmail.com>
2023-11-28 19:49:31 +0300

    Update GameController.cpp

--- GameController.cpp
+++ GameController.cpp
@@ -22,6 +22,7 @@ bool GameController::play(BlockFall &game, const string &commands_file) {
     // TODO: Implement the gameplay here while reading the commands from the input file given as the 3rd command-line
     //       argument. The return value represents if the gameplay was successful or not: false if game over,
     //       true otherwise.
+    game.gameEnd = 0;
 
     fstream inputFile(commands_file);
     if (!inputFile.is_open()) {
